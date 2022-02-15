3d1aadc2201d35d8096daee04e561d5dcd2b6141
pkbims <bimal_home@Bimals-MacBook-Pro.local>
2022-02-14 22:50:07 -0500

    aa

--- TCO2_Dashboard/import_data_2.py
+++ TCO2_Dashboard/import_data_2.py
@@ -1,33 +1,33 @@
-from subgrounds.subgrounds import to_dataframe
+
 from subgrounds.subgrounds import Subgrounds
 
 
 
-                      
-sg = Subgrounds()
-carbon_data = sg.load_subgraph('https://api.thegraph.com/subgraphs/name/cujowolf/polygon-bridged-carbon')
+def get_data_2():
+    sg = Subgrounds()
+    carbon_data = sg.load_subgraph('https://api.thegraph.com/subgraphs/name/cujowolf/polygon-bridged-carbon')
 
-carbon_offsets = carbon_data.Query.carbonOffsets(
-orderBy=carbon_data.CarbonOffset.lastUpdate,
-orderDirection='desc',
-first = 4999
-)
+    carbon_offsets = carbon_data.Query.carbonOffsets(
+    orderBy=carbon_data.CarbonOffset.lastUpdate,
+    orderDirection='desc',
+    first = 4999
+    )
 
-req = sg.query_df([
-carbon_offsets.tokenAddress,
-    carbon_offsets.region,
-    carbon_offsets.vintage,
-    carbon_offsets.projectID,
-    carbon_offsets.standard,
-    carbon_offsets.methodology,
-    carbon_offsets.balanceBCT,
-    # carbon_offsets.balanceNCT,
-    carbon_offsets.totalBridged,
-    carbon_offsets.retirements.value
+    req = sg.query_df([
+    carbon_offsets.tokenAddress,
+        carbon_offsets.region,
+        carbon_offsets.vintage,
+        carbon_offsets.projectID,
+        carbon_offsets.standard,
+        carbon_offsets.methodology,
+        carbon_offsets.balanceBCT,
+        # carbon_offsets.balanceNCT,
+        carbon_offsets.totalBridged,
+        carbon_offsets.retirements.value
+    ])
 
-])
-
-# data = sg.execute(req)
-print(req)
-df = to_dataframe(data)
-print(df)
+    # data = sg.execute(req)
+    
+    # df = to_dataframe(data)
+    # print(df)
+    return req
